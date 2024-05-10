f611739b424ab20c9e80026d4dda4a00c721e00f
Elsumir <44923793+Elsumir@users.noreply.github.com>
2024-05-10 11:40:25 +0300

    добавил парсинг событий

--- index.js
+++ index.js
@@ -1,26 +1,89 @@
 const TelegramBot = require('node-telegram-bot-api');
-const axios = require('axios');
-const express = require('express');
-const app = express();
+const fs = require('fs');
+let rawdata = fs.readFileSync('my.json');
+let parseddata = JSON.parse(rawdata);
 
-app.get('/', (req, res) => {
-  res.send('Bot is alive');
-});
+const url =
+  'https://1xstavka.ru/LiveFeed/Get1x2_VZip?sports=6&count=50&gr=44&antisports=188&mode=4&country=1&partner=51';
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const botToken = process.env.BOT;
+const chatId = process.env.CHAT;
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.BOT;
+const messageForBot = async (messageText) => {
+  await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: messageText,
+    }),
+  });
 
-const bot = new TelegramBot(token, { polling: true });
+  console.log('Есть событие!');
+};
+
+const sendTelegramMessage = async (result) => {
+  if (!result) {
+    const messageText = 'Не работаю';
+    messageForBot(messageText);
+  } else {
+    const { id, liga, partyOne, partyTwo, sets } = result;
+    const noDubl = parseddata.filter((e) => e.id === id).length;
+
+    if (noDubl) {
+      return;
+    }
+    const messageText = `Лига: ${liga} | К1: ${partyOne} | К2: ${partyTwo} | Счет: ${sets}`;
+    messageForBot(messageText);
+
+    await parseddata.push({
+      id,
+    });
+    let data = JSON.stringify(parseddata);
+    fs.writeFileSync('my.json', data);
+  }
+};
+
+const searchEvent = () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const championship = data.Value.filter((e) => e.L.includes('Женщины'));
+
+      championship.forEach((obj) => {
+        const set = obj.SC.PS.map((e) => e.Value).map((e) => e.S1 + e.S2);
+
+        let over = 0;
+        set.forEach((e) => {
+          e > 44 || over > 1 ? (over += 1) : (over = 0);
+
+          if (over > 1 && set.length < 4) {
+            const result = {
+              id: obj.I,
+              liga: obj.L,
+              partyOne: obj.O1,
+              partyTwo: obj.O2,
+              sets: set,
+            };
+            sendTelegramMessage(result);
+          }
+        });
+      });
+      console.log('Работаю!');
+    })
+    .catch(() => sendTelegramMessage());
+};
+// res();
+setInterval(searchEvent, 60000);
+
+const bot = new TelegramBot(botToken, { polling: true });
 
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Работаю!');
 });
 
 module.exports = app;
+
+
